fdc1f890d7f2fdb41d3bae71c113cce26cdd4c44
djq314159 <150891394+djq314159@users.noreply.github.com>
2024-05-13 17:13:37 -0500

    Create __init__.py

---
+++ statassist/__init__.py
@@ -0,0 +1,8 @@
+from StatAssist import MATH, ALGORITHMS
+algorithms = ALGORITHMS
+integrals = MATH.CALCULUS
+stats = MATH.STATS
+algebra = MATH.ALGEBRA
+
+__version__ = '1.0.0'
+__author__ = 'Damion Jay Quintanilla'
