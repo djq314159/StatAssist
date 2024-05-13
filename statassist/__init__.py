84f73454eda3716907c0bcfdb0e2b2e120836181
djq314159 <150891394+djq314159@users.noreply.github.com>
2024-05-13 17:16:40 -0500

    Delete statassist directory

--- statassist/__init__.py
+++
@@ -1,8 +0,0 @@
-from StatAssist import MATH, ALGORITHMS
-algorithms = ALGORITHMS
-integrals = MATH.CALCULUS
-stats = MATH.STATS
-algebra = MATH.ALGEBRA
-
-__version__ = '1.0.0'
-__author__ = 'Damion Jay Quintanilla'
